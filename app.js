61d1a79bec2b77dfd66f3503324aa017e5a7dc5e
makoto.tomitani <makoto.tomitani@gmail.com>
2021-01-12 22:31:01 +0900

    静的ファイルの配信先設定

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
+<<<<<<< Updated upstream
 // コンソールログを出力する
 console.log(process.env.NODE_ENV);
+=======
+const express = require("express");
+const app =express();
+
+app.set("view engine", "ejs");
+app.disable("x-powered-by"); // 無効化処理
+
+// 静的ファイルの配信
+app.use("/public", express.static(__dirname + "/public/" + (process.env.NODE === "prod" ? "production" : "development")));
+
+// ルーターへのパス
+app.use("/", require("./routes/index.js"));
+
+console.log("Node ポート番号:3000 起動 ========== ");
+app.listen(3000);
+>>>>>>> Stashed changes
